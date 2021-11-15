a38bef63c40b3c2d38224ac2e9843081553d289b
Ben Beecher <Ben@lightmatter.com>
2021-11-15 16:57:33 -0500

    playing around with vue

--- frontend/js/system.js
+++ frontend/js/system.js
@@ -1,6 +1,55 @@
 import 'htmx.org';
-import {createApp} from 'petite-vue';
+import {createApp, reactive} from 'petite-vue';
 
-htmx.on("htmx:load", function(evt) {
-  createApp().mount(evt.detail.elt);
+const store = reactive({
+  count: 0,
+  global_inc() {
+    this.count++
+  }
+})
+
+
+function GlobalCounter(props) {
+  return {
+    count: props.initialCount,
+    inc() {
+      store.inc();
+    },
+    mounted() {
+      console.log(`I'm mounted!`);
+    }
+  };
+}
+
+
+function Counter(props) {
+  return {
+    count: props.initialCount,
+    inc() {
+      this.count++
+    },
+    mounted() {
+      console.log(`I'm mounted!`);
+    }
+  };
+}
+
+
+// manipulate it here
+store.global_inc();
+
+
+const app = createApp({
+  store,
+  GlobalCounter,
+  Counter,
+  $delimiters: ['${', '}']
 });
+//catch template jitter
+app.mount(document.body);
+htmx.on("htmx:afterSwap", function(evt) {
+  app.mount(evt.target);
+});
+// htmx.on("htmx:load", function(evt) {
+//     app.mount(evt.detail.elt);
+//  });
